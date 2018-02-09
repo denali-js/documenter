61e6957d607d88af8cb48e4f4d8bc37e4060de09
Dave Wasmer <davewasmer@gmail.com>
2018-02-08 21:52:53 -0700

    fix: fix types for extracter tree options

--- src/trees/extracter.ts
+++ src/trees/extracter.ts
@@ -5,10 +5,6 @@ import * as path from 'path';
 
 export interface ExtracterTreeOptions {
   /**
-   * The root directory of the project to extract documentation from
-   */
-  dir: string;
-  /**
    * The path to the directory containing all the Pages to build, relative to `dir`. Defaults to `docs`
    */
   pagesDir?: string;
@@ -17,35 +13,23 @@ export interface ExtracterTreeOptions {
    * Defaults to `src`
    */
   sourceDirs?: string[];
+  /**
+   * The name of the project we are extracting docs from
+   */
+  projectName: string;
+  /**
+   * The version string of the project we are extracting docs from
+   */
+  projectVersion: string;
 }
 
 export default class ExtracterTree extends Tree {
 
-  options: {
-    /**
-     * The name of the project we are extracting docs from
-     */
-    projectName: string;
-    /**
-     * The version string of the project we are extracting docs from
-     */
-    projectVersion: string;
-    /**
-     * The path to the directory containing all the Pages to build, relative to `dir`. Defaults to `docs`
-     */
-    pagesDir: string;
-    /**
-     * An array of paths to the directories containing the source code to extract API docs from.
-     * Defaults to `src`
-     */
-    sourceDirs: string[];
-  };
+  options: ExtracterTreeOptions;
 
   constructor(sourceTree: Tree | string, options: ExtracterTreeOptions) {
     super([ sourceTree ]);
-    options.sourceDirs = options.sourceDirs || [ 'src' ];
-    options.pagesDir = options.pagesDir || 'docs';
-    this.options = <any>options;
+    this.options = options;
   }
 
   build() {
